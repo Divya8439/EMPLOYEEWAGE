deb4839f9d3b70488302f6f2fcfa43ca8e02ad87
Divya8439 <divyareddy.8439@gmail.com>
2022-12-07 11:42:25 +0530

    2

---
+++ Employeepracticeproblem/uc1empattendance.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x 
+isPresent=1;
+randomCheck=$((RANDOM%2));
+
+if [ $isPresent1  -eq $randomCheck0 ];
+then 
+
+        echo "Employee is Present";
+else
+        echo "Employee is Abscent";
+fi
